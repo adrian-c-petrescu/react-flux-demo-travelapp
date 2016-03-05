062a9c35b2aaa4c862a503fe38d3c43df3d385ad
adrian-c-petrescu <adrian.c.petrescu@gmail.com>
2016-03-05 16:15:28 +0000

    WebApi for data retrieval

---
+++ api/Hotels/App_Start/DiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Hotels.Code.Logic;
+using Hotels.Code.Persistence;
+
+namespace Hotels.App_Start
+{
+    public class DiConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<HotelQueryLogic>().As<IHotelQueryLogic>().InstancePerRequest();
+            builder.RegisterType<HotelsDataSource>().As<IHotelsDataSource>().SingleInstance();
+            
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ api/Hotels/Models/HotelsDataQuery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Hotels.Models
+{
+    public class HotelsDataQuery
+    {
+        public Pagination Pagination { get; set; }
+        public IEnumerable<DataFilter> Filters { get; set; }
+        public Sort Sort { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int Start { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class DataFilter
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public DataFilterType Type { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Sort
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public DataFilterType Field { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Direction Direction { get; set; }
+    }
+
+    public enum DataFilterType
+    {
+        Name,
+        Stars,
+        UserRating,
+        MinCost,
+        Distance
+    }
+
+    public enum Direction
+    {
+        Asc,
+        Desc
+    }
+}
